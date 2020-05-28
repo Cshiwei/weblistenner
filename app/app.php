35aaa8b44f21b04db7f07cab2e6e9a5250371704
shiwei.cao <shiwei.cao@yingzt.com>
2020-05-28 18:48:43 +0800

    ok

---
+++ app/app.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: csw
+ * Date: 2020/5/28
+ * Time: 17:35
+ */
+
+require_once __DIR__.'/lib/Autoloader.php';
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/helper/common.php';
